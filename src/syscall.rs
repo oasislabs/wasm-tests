473dab34c87b59bf52a8559dc171ffab0e4d3a11
Nick Hynes <nhynes@oasislabs.com>
2019-02-28 17:58:56 +0000

    Improve syscall abi

--- src/syscall.rs
+++ src/syscall.rs
@@ -3,9 +3,13 @@ extern crate serde;
 #[derive(serde::Deserialize)]
 struct TestInput {
 	syscall_index: usize,
-	payload: Vec<u32>,
-	data: Vec<u8>,
-	relocations: Vec<usize>,
+	input: Vec<InputItem>,
+}
+
+#[derive(serde::Deserialize)]
+enum InputItem {
+	Prim(u32),
+	Blob(Vec<u8>),
 }
 
 extern "C" {
@@ -15,10 +19,10 @@ extern "C" {
 #[no_mangle]
 pub unsafe fn call() {
 	let test_input: TestInput = serde_cbor::from_slice(&owasm_ethereum::input()).unwrap();
-	let mut payload = test_input.payload;
-	for reloc in test_input.relocations.iter() {
-		payload[*reloc] += test_input.data.as_ptr() as u32;
-	}
+	let mut payload: Vec<u32> = test_input.input.iter().map(|inp| match inp {
+		InputItem::Prim(val) => *val,
+		InputItem::Blob(ref data) => data.as_ptr() as u32,
+	}).collect();
 	rust_wasm_syscall(test_input.syscall_index, payload.as_mut_ptr() as *mut u8);
 	let ret_data = std::slice::from_raw_parts(
 		payload.as_ptr() as *const u8,
